5566208a42e6431edb91c7969ba8f875f5595104
Jose-Otero-M <otero.martinez.jose.2000@gmail.com>
2026-06-12 15:00:45 -0600

    Improved comments on srl16e_delay_line.v

--- rtl/srl16e_delay_line.v
+++ rtl/srl16e_delay_line.v
@@ -76,36 +76,5 @@ module srl16e_delay_line #(
             end
         end
     endgenerate
-
-/*    
-    parameter integer DATA_W = 16,
-    parameter [3:0]   DELAY_SEL = 4'd15
-)(
-    input  wire              clk,
-    input  wire              ce,
-    input  wire [DATA_W-1:0] din,
-    output wire [DATA_W-1:0] dout
-);
-
-    genvar bit_idx;
-
-    generate
-        for (bit_idx = 0; bit_idx < DATA_W; bit_idx = bit_idx + 1) begin : gen_srl16e
-
-            SRL16E #(
-                .INIT(16'h0000)
-            ) u_srl16e (
-                .Q   (dout[bit_idx]),
-                .A0  (DELAY_SEL[0]),
-                .A1  (DELAY_SEL[1]),
-                .A2  (DELAY_SEL[2]),
-                .A3  (DELAY_SEL[3]),
-                .CE  (ce),
-                .CLK (clk),
-                .D   (din[bit_idx])
-            );
-
-        end
-    endgenerate
-*/
+    
 endmodule
